3586b1c71cca5d78c6dbbd7e96988c331e6282c3
Yurii Fitsyk <yuriy.fitsyk@gmail.com>
2021-01-21 23:00:54 +0200

    Updates

---
+++ precache-manifest.c51e48719bfbaf9ca8cd0cd849451922.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bfe4386b69d90d260d9ca94e9eb7a41",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3a9fde03a997121f9a42",
+    "url": "./static/css/2.9634ee28.chunk.css"
+  },
+  {
+    "revision": "a7c910da917888838b32",
+    "url": "./static/css/main.bada76a3.chunk.css"
+  },
+  {
+    "revision": "3a9fde03a997121f9a42",
+    "url": "./static/js/2.d10b847f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.d10b847f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7c910da917888838b32",
+    "url": "./static/js/main.06fc5978.chunk.js"
+  },
+  {
+    "revision": "79d17474afd20bce64b6",
+    "url": "./static/js/runtime-main.12497659.js"
+  }
+]);
